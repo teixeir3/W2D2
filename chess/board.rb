74f0c5c9376d60afb0a73e2707c44e209d21b149
Doug and Katie <doug&katie@appacademy.io>
2014-01-07 13:30:56 -0500

    Fixes bugs in SlidingPiece#moves Rook#path

--- chess/board.rb
+++ chess/board.rb
@@ -1,13 +1,91 @@
 class Board
+  attr_reader :rows
 
-  def initialize
+  def self.blank_grid
+    Array.new(8) { Array.new(8) }
+  end
 
+  def initialize(rows = self.class.blank_grid)
+    @rows = rows
+  end
+
+  def [](pos)
+    # returns piece object at that pos
+    x, y = pos[0], pos[1]
+    @rows[x][y]
+  end
+
+  def []=(pos, mark)
+    # ADD Error handling
+    x, y = pos[0], pos[1]
+    @rows[x][y] = mark
   end
 
   def move(start_pos, end_pos)
     # calls move on piece at the start_pos and passes end_pos
     # handles exceptions / errors
+    # updates the board with piece in new positon
+  end
+
+  def dup
+    duped_rows = rows.map(&:dup)
+    self.class.new(duped_rows)
+  end
+
+  def empty?(pos)
+    self[pos].nil?
   end
 
 
 end
+
+ # def cols
+#    cols = [[], [], []]
+#    @rows.each do |row|
+#      row.each_with_index do |mark, col_idx|
+#        cols[col_idx] << mark
+#      end
+#    end
+#
+#    cols
+#  end
+
+ # def diagonals
+#    down_diag = [[0, 0], [1, 1], [2, 2]]
+#    up_diag = [[0, 2], [1, 1], [2, 0]]
+#
+#    [down_diag, up_diag].map do |diag|
+#      # Note the `x, y` inside the block; this unpacks, or
+#      # "destructures" the argument. Read more here:
+#      # http://tony.pitluga.com/2011/08/08/destructuring-with-ruby.html
+#      diag.map { |x, y| @rows[x][y] }
+#    end
+#  end
+
+  # def tied?
+#     return false if won?
+#
+#     # no empty space?
+#     @rows.all? { |row| row.none? { |el| el.nil? }}
+#   end
+#
+#   def over?
+#     # style guide says to use `or`, but I (and most others) prefer to
+#     # use `||` all the time. We don't like two ways to do something
+#     # this simple.
+#     won? || tied?
+#   end
+#
+#   def winner
+#     (rows + cols + diagonals).each do |triple|
+#       return :x if triple == [:x, :x, :x]
+#       return :o if triple == [:o, :o, :o]
+#     end
+#
+#     nil
+#   end
+#
+#   def won?
+#     !winner.nil?
+#   end
+
--- chess/piece.rb
+++ chess/piece.rb
@@ -8,6 +8,10 @@ class Piece
     @color = color
   end
 
+  def to_s
+    "#{@color} #{self.class}"
+  end
+
 end
 
 
@@ -15,7 +19,25 @@ end
 
 class SlidingPiece < Piece
   def moves(end_pos)
-    move_dirs(end_pos)
+    path(end_pos).each do |path_pos|
+      # check to see if there is another piece in the path
+      unless @board[path_pos].nil?
+        # If piece is your own color, your move is blocked
+        raise InvalidMoveError if @board[path_pos].color == @color
+        # if the piece is of the other color...
+        if @board[path_pos].color != @color
+          # you can take it if you are the end of your path
+          if path_pos == end_pos
+            @board[path_pos].remove
+          else
+            # otherwise you are blocked by that piece
+            raise InvalidMoveError
+          end
+        end
+      end
+      @pos = end_pos
+    end
+
 
     # it wouldn't be valid if there is a piece of the same color
     # if it's a piece of the opposing color, you can move to there but not past
@@ -23,6 +45,8 @@ class SlidingPiece < Piece
   end
 
   def remove
+    @pos = nil
+    true
     # this gets called by a taking piece on the piece it takes
     # maybe this should be a board method?
   end
@@ -30,26 +54,27 @@ end
 
 class Rook < SlidingPiece
 
-  def move_dirs(end_pos)
+  def path(end_pos)
     x_end,y_end = end_pos
     path = []
 
     if x_end != @pos[0]
-      (@pos[0]..7).each do |x_pos|
+      (@pos[0]+1..7).each do |x_pos|
         path << [x_pos,@pos[1]]
-        return path if path.last == end_pos
       end
-      raise InvalidMoveError
     elsif y_end != @pos[0]
-      (@pos[1]..7).each do |y_pos|
+      (@pos[1]+1..y_end).each do |y_pos|
         path << [@pos[0], y_pos]
-        return path if path.last == end_pos
       end
-      raise InvalidMoveError
     else
       raise InvalidMoveError
     end
 
+    if path.last == end_pos
+      return path
+    else
+      raise InvalidMoveError
+    end
   end
 
 end
