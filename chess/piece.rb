18a821625620f3a7ce31aa577921c567bf8f439f
Doug and Katie <doug&katie@appacademy.io>
2014-01-07 11:45:16 -0500

    Refactors Rook#move_dirs to return valid path arr

--- chess/piece.rb
+++ chess/piece.rb
@@ -15,11 +15,8 @@ end
 
 class SlidingPiece < Piece
   def moves(end_pos)
+    move_dirs(end_pos)
 
-    raise InvalidMoveError unless move_dirs.include?(end_pos)
-
-    # move needs to check which directions it can move in
-    # check each position in that direction to see if it's a valid move
     # it wouldn't be valid if there is a piece of the same color
     # if it's a piece of the opposing color, you can move to there but not past
     # the other piece needs to get removed from the board if so
@@ -35,29 +32,25 @@ class Rook < SlidingPiece
 
   def move_dirs(end_pos)
     x_end,y_end = end_pos
+    path = []
 
     if x_end != @pos[0]
       (@pos[0]..7).each do |x_pos|
-        return if [x_pos,@pos[1]] == end_pos
+        path << [x_pos,@pos[1]]
+        return path if path.last == end_pos
       end
       raise InvalidMoveError
     elsif y_end != @pos[0]
       (@pos[1]..7).each do |y_pos|
-        return if [@pos[0], y_pos] == end_pos
+        path << [@pos[0], y_pos]
+        return path if path.last == end_pos
       end
+      raise InvalidMoveError
     else
       raise InvalidMoveError
     end
-  end
 
-    # returns list of reachable squares regardless of other pieces' positions
-    # reachable_pos_arr = []
-#     (0..7).each do |num|
-#       reachable_pos_arr << [pos[0], num]
-#       reachable_pos_arr << [num, pos[1]]
-#     end
-#     reachable_pos_arr
-#   end
+  end
 
 end
 
