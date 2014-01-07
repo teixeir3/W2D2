5279205561ec28a3c53ff90b0feb6c8375e7aa6d
Doug and Katie <doug&katie@appacademy.io>
2014-01-07 10:04:07 -0500

    Adds Manager#bonus & Manager#set_bosses

--- employee/employee.rb
+++ employee/employee.rb
@@ -1,16 +1,24 @@
 class Employee
 
-  attr_reader :name, :title, :salary, :boss
+  attr_reader :name, :title, :salary
+  attr_accessor :boss
 
-  def initialize(attributes)
+  def initialize(attributes = {})
+    defaults = {
+      :title => "Coder",
+      :salary => 100_000
+    }
+    attributes = defaults.merge(attributes)
+
+
     @name = attributes[:name]
     @title = attributes[:title]
     @salary = attributes[:salary]
-    @boss = attributes[:boss]
+    @boss = nil
   end
 
   def bonus(multiplier)
-
+    bonus = @salary * multiplier
   end
 
 end
@@ -21,8 +29,27 @@ class Manager < Employee
   def initialize(subordinates, attributes)
     super(attributes)
     @subordinates = subordinates
+    set_bosses
   end
 
+  def bonus(multiplier)
+    total_salary = 0
+    employee_queue = [self]
+    until employee_queue.empty?
+      current_employee = employee_queue.shift
+      total_salary += current_employee.salary
+      if current_employee.methods.include?(:subordinates)
+        employee_queue += current_employee.subordinates
+      end
+    end
+    total_salary * multiplier
+  end
+
+  def set_bosses
+    @subordinates.each do |subordinate|
+      subordinate.boss = self
+    end
+  end
 
 
 end
