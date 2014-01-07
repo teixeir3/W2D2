04fc825f1a8ea54915ee17b5f7924d212b5ebced
Doug and Katie <doug&katie@appacademy.io>
2014-01-07 11:12:59 -0500

    Adds board.rb with skeleton of #move

---
+++ board.rb
@@ -0,0 +1,13 @@
+class Board
+
+  def initialize
+
+  end
+
+  def move(start_pos, end_pos)
+    # calls move on piece at the start_pos and passes end_pos
+    # handles exceptions / errors
+  end
+
+
+end
